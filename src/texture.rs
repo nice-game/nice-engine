cd247c4b93a72370950a8c96237e8ed1fd0a1dc2
IcyDefiance <icydefiance@gmail.com>
2019-08-09 18:57:54 -0400

    Add target texture

--- src/texture.rs
+++ src/texture.rs
@@ -1,9 +1,11 @@
 mod immutable;
+mod target;
 
 pub use immutable::ImmutableTexture;
+pub use target::TargetTexture;
 
-use vulkano::image::ImageViewAccess;
 use std::sync::Arc;
+use vulkano::image::ImageViewAccess;
 
 pub trait Texture {
 	fn image(&self) -> &Arc<dyn ImageViewAccess + Send + Sync>;
---
+++ src/texture/target.rs
@@ -0,0 +1,26 @@
+use super::Texture;
+use std::sync::Arc;
+use vulkano::{
+	device::Device,
+	format::FormatDesc,
+	image::{AttachmentImage, ImageCreationError, ImageViewAccess},
+};
+
+#[derive(Clone)]
+pub struct TargetTexture {
+	image: Arc<dyn ImageViewAccess + Send + Sync>,
+}
+impl TargetTexture {
+	pub(crate) fn new<F>(device: Arc<Device>, dimensions: [u32; 2], format: F) -> Result<Self, ImageCreationError>
+	where
+		F: FormatDesc + Send + Sync + 'static,
+	{
+		let image = AttachmentImage::sampled(device, dimensions, format)?;
+		Ok(Self { image })
+	}
+}
+impl Texture for TargetTexture {
+	fn image(&self) -> &Arc<dyn ImageViewAccess + Send + Sync> {
+		&self.image
+	}
+}
